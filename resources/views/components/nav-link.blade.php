c52910b7ab4051d272af5c5105165cb140e0e0cc
Humaun Kabir <Humaunkabir5561@gmail.com>
2025-07-17 01:07:53 +0600

    new

---
+++ resources/views/components/nav-link.blade.php
@@ -0,0 +1,6 @@
+
+     <nav>
+    <a href="<?php echo route('home');?>">Home</a>
+    <a href="{{ Route('about') }}">About</a>
+    <a href="{{ Route('contact') }}">Contact</a>
+    </nav>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+     <x-nav-link></x-nav-link>
+
+      <div>
+        Welcome to Contact
+    </div>
+</body>
+</html>
